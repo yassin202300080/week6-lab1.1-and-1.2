04d764e4876f59ae8a62d001716c509945f281de
yassin <ye2300080@tkh.edu.eg>
2025-11-04 09:59:13 +0200

    updating authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,16 @@
+const { db } = require('../db.js');
+// POST /signup
+const signup = (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const role = 'user'; // default to non-admin
 
+  if (!name || !email || !password) {
+    return res.status(400).send('Please provide name, email, and password');
+  }
+
+  const query = `
+    INSERT INTO user (NAME, EMAIL, ROLE, PASSWORD)
+    VALUES ('${name}', '${email}', '${role}', '${password}')
+  `;
