17d87f8adf23041198eee48768338283d21088fd
yassin <ye2300080@tkh.edu.eg>
2025-11-04 10:25:32 +0200

    sign up

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,3 +14,22 @@ const signup = (req, res) => {
     INSERT INTO user (NAME, EMAIL, ROLE, PASSWORD)
     VALUES ('${name}', '${email}', '${role}', '${password}')
   `;
+
+
+  const { db } = require('../db.js');
+
+// POST /signup
+const signUp = (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const role = 'user'; // default to non-admin
+
+  if (!name || !email || !password) {
+    return res.status(400).send('Please provide name, email, and password');
+  }
+
+  const query = `
+    INSERT INTO USER (NAME, EMAIL, ROLE, PASSWORD)
+
+    
