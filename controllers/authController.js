de6f44e6a7125e135f3b772d794ccc7c3f4a9c01
yassin202300080 <yassin.elkholy@gmail.com>
2025-11-05 10:00:49 +0200

    login query

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,3 +32,50 @@ VALUES ('${name}', '${email}', '${role}', '${hashedPassword}')
 
     return res.status(200).send('Registration successful');
   });
+
+    const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const { db } = require('../db.js');
+
+const signInToken = (id, role) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const login = (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(400).send('Please provide email and password.');
+  }
+
+  const query = `SELECT * FROM USER WHERE EMAIL='${email}'`;
+
+  db.get(query, (err, row) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Database error');
+    }
+
+const login = (req, res) => {
+  db.get(query, (err, row) => {
+    bcrypt.compare(password, row.PASSWORD, (err, isMatch) => {
+      const token = signInToken(row.ID, row.ROLE);
+
+      return res.status(200).json({
+        message: 'Login successful',
+        user: {
+          id: row.ID,
+          name: row.NAME,
+          email: row.EMAIL,
+          role: row.ROLE,
+        },
+        token,
+      });
+    });
+  });
+};
+
+
